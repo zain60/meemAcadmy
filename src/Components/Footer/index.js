3987b04c71647c142271202076e2673dbb295aec
zain <zainengr00@gmail.com>
2024-04-22 20:15:29 +0500

    social links added

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -90,7 +90,7 @@ const Footer = () => {
             </p>
 
             <p className="mt-4 text-sm text-gray-500 sm:order-first sm:mt-0">
-              &copy; 2023 BlockwebTech
+              &copy; 2024 code Axis
             </p>
           </div>
         </div>
