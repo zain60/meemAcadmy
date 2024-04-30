b2229f3cd19c258f4935eef89b63258497371da6
zain <zainengr00@gmail.com>
2024-04-30 22:32:36 +0500

    udated dynamic id code

--- src/app/Courses/[name]/page.js
+++ src/app/Courses/[name]/page.js
@@ -1,19 +1,15 @@
 "use client";
-// Pages/Courses/[name].js
-import { usePathname } from "next/navigation";
 import React from "react";
 import CourseDetails from "../../../Components/Courses/CourseDeatils";
 import courseDetailsData from "../../../Data/corseData.json";
 
-const CoursePage = () => {
-  const navigation = usePathname();
-  const pathnameParts = navigation.split("/");
-  const name = pathnameParts[pathnameParts.length - 1];
+const CoursePage = ({params}) => {
+  console.log({params})
 
   const selectedCourseDetails = courseDetailsData.find(
     (course) =>
       course.courseTitle &&
-      course.courseTitle.toLowerCase() === name.toLowerCase()
+      course.courseTitle.toLowerCase() === params.name
   );
 
   return (
